9089b76053c2d1122df390d0a592c33027a4845f
Horace-G <horacio.galdamez@unitec.edu>
2014-03-07 00:29:42 -0600

    implementando la clase de las aristas

---
+++ RutaArista.java
@@ -0,0 +1,37 @@
+package project3;
+
+/**
+ *
+ * @author Horacio Galdamez
+ */
+
+/*
+ * En ésta clase, representamos las aristas, objetos que contendran ambos la aerolínea que la realiza, así como
+ * el precio de la misma.
+ */
+
+public class RutaArista {
+
+    ////Atributos////
+    private String airline;//aerolínea que ejecuta el vuelo
+    private double cost;//costo de ése vuelo
+
+    public RutaArista(String airline, double cost) {//ctor de la clase
+        this.airline = airline;
+        this.cost = cost;
+    }//fin del ctor
+
+    public String getAirline() {//método accesor de la aerolínea
+        return airline;
+    }//fin del método getAirline
+
+    public double getCost() {//Retorna el costo de traversar la arista
+        return cost;
+    }//fin del método getCost
+
+    @Override
+    public String toString() {//creamos nuestro propio toString para mejorar la impresión presentada al usuario
+        return airline + ", $" + cost;
+    }//fin del metodo toString
+
+}//fin de la clase RutaArista
